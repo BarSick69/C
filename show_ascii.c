8a92ddfb0ce22ca8aca259d41b32d1fb9fcacee8
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2024-05-13 15:27:22 +0300

    Update show_ascii.c
    other changes to show value.Also set OPOST to 0,because it turns every \n into \r\n,where \r moves the cursor to the left

--- show_ascii.c
+++ show_ascii.c
@@ -11,7 +11,8 @@ void enableRawMode(){
     tcgetattr(STDIN_FILENO,&orig_termios);
     atexit(disableRawMode);
     struct termios raw=orig_termios;
-     raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_oflag &= ~(OPOST);
     raw.c_lflag &= ~(ECHO|ICANON|ISIG|IEXTEN);
 
     tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
@@ -22,9 +23,9 @@ int main(){
     enableRawMode();
     while (read(STDIN_FILENO,&c,1)==1 && c != 'q'){
            if(iscntrl(c)){
-               printf("%d\n",c);
+               printf("%d\r\n",c);
            }else{
-            printf("%d ('%c')\n",c,c);
+            printf("%d ('%c')\r\n",c,c);
            }
     };
 
